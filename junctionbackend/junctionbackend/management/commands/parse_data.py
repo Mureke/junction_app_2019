32ce5a2686e57909995269dfe1a86a6e0615abca
Mureke <markus.suomalainen@gmail.com>
2019-11-16 00:18:47 +0200

    Trails mocking in parse data

--- junctionbackend/junctionbackend/management/commands/parse_data.py
+++ junctionbackend/junctionbackend/management/commands/parse_data.py
@@ -4,6 +4,22 @@ import csv
 from django.db.utils import IntegrityError
 from junctionbackend.models import ParkVisits, Trail, NationalPark
 import datetime
+from junctionbackend.utils import geo_converter
+from django.contrib.gis.geos.point import Point
+
+PARK_NAMES = {
+    "852": 'Nuuksio National Park',
+    "34361": 'Pallas-Yllästunturi National Park'
+}
+
+TRAILS = {
+    "1099": {
+        "name": "Varkaankuru",
+        "location": geo_converter.convert_espg3067(7500782, 381166),
+        "national_park_id": "34361"
+    }
+}
+
 
 class Command(BaseCommand):
     help = "Parse Data"
@@ -14,7 +30,6 @@ class Command(BaseCommand):
             help='Upload a csv for parsing to conditions_translations'
         )
 
-
     def handle(self, *args, **options):
         new_entries = 0
         skipped_entries = 0
@@ -24,7 +39,11 @@ class Command(BaseCommand):
                 reader = csv.DictReader(f, dialect=CustomDialect)
                 for row in reader:
                     try:
-                        trail = Trail.objects.filter(counter_id_asta=row['CounterID_ASTA']).first()
+                        national_park = NationalPark.objects.get_or_create(national_park_code=row['ASTA_Counters.NationalParkCode'], name=PARK_NAMES[row['ASTA_Counters.NationalParkCode']])
+                        lat, long = geo_converter.convert_espg3067(row['PAVE_Counters.CoordinateNorth'], row['PAVE_Counters.CoordinateEast'])
+                        trail = Trail.objects.get_or_create(counter_id_asta=row['CounterID_ASTA'], name=row['ASTA_Counters.Name_ASTA'],
+                                                            location=Point(x=lat, y=long),
+                                                            national_park=national_park[0])
                         start_date = datetime.datetime.strptime(row['StartTime'], '%d/%m/%Y %H:%M')
                         end_date = datetime.datetime.strptime(row['StartTime'], '%d/%m/%Y %H:%M')
                         park_visit = ParkVisits(start_time=start_date, end_time=end_date, visits=row['Visits'], trail=trail)
@@ -48,6 +67,7 @@ class Command(BaseCommand):
 
         print("Added " + str(new_entries) + " new entries. Skipped " + str(skipped_entries) + " existing entries")
 
+
 class CustomDialect(csv.Dialect):
     delimiter = ';'
     quotechar = '"'
@@ -58,4 +78,3 @@ class CustomDialect(csv.Dialect):
 
 
 csv.register_dialect("custom", CustomDialect)
-
