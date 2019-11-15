a647d375e6e00c6d35f29d97c7ac38e9e17fe5cf
Mureke <markus.suomalainen@gmail.com>
2019-11-15 22:06:12 +0200

    Small fix to geoconverter

--- junctionbackend/junctionbackend/utils/geo_converter.py
+++ junctionbackend/junctionbackend/utils/geo_converter.py
@@ -1,16 +1,16 @@
 from pyproj import Proj, transform
 
 
-def conver_espg3067(north, east):
+def convert_espg3067(north, south):
     """
     Converts espg2067 to normal latlan
     :param north:
-    :param east:
-    :return: [n, e]
+    :param south:
+    :return: [n, s]
     """
     inProj = Proj(init='epsg:3067')
     outProj = Proj(init='epsg:4269')
-    x1, y1 = (north, east)
-    x2, y2 = transform(inProj, outProj, y1, x1)
+    x1, y1 = (north, south)
+    north, south = transform(inProj, outProj, y1, x1)
 
-    return y2, x2
+    return north, south
