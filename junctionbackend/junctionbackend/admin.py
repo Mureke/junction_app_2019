1673d8d107996aee303e45c1f26ed624eef123de
Mureke <markus.suomalainen@gmail.com>
2019-11-16 12:37:23 +0200

    Tag admin improvements

--- junctionbackend/junctionbackend/admin.py
+++ junctionbackend/junctionbackend/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+from django.forms import ModelForm, CharField, URLField
 from junctionbackend.models import Tag
 
 
+class TagForm(ModelForm):
+    name = CharField(required=True)
+    icon_url = CharField(required=False)
+
+
 class TagAdmin(admin.ModelAdmin):
-    pass
+    form = TagForm
+    list_display = ('name', )
 
 
 admin.site.register(Tag, TagAdmin)
